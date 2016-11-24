03ef5604b77ac80f55269bae8c73dac202ce809c
marcovaccarini <quirkmode@yahoo.com>
2016-11-24 22:46:14 +0100

    edit TicketsController

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class TicketsController extends Controller
 {
-    //
+    public function create()
+    {
+        $categories = Category::all();
+
+        return view('ticket.create', compact('categories'));
+
+    }
 }
