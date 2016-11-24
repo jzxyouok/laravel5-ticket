d5d5f5b092cb56cc999dbdd9446b225453193c18
marcovaccarini <quirkmode@yahoo.com>
2016-11-24 22:17:28 +0100

    add ticket migration

--- database/migrations/2016_11_24_210930_create_tickets_table.php
+++ database/migrations/2016_11_24_210930_create_tickets_table.php
@@ -15,6 +15,13 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->integer('ticket_id')->unique;
+            $table->string('title');
+            $table->string('priority');
+            $table->text('message');
+            $table->string('status');
             $table->timestamps();
         });
     }
